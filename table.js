3362809406c8be6007a1b8d1b2311045fcbfd6aa
Dor Rubin <dor.rubin@gmail.com>
2015-07-09 10:33:21 -0400

    adding rows to table

---
+++ table.js
@@ -0,0 +1,42 @@
+angular.module('User', [])
+	. controller('UserController', ['$scope', function($scope){
+		$scope.users = [
+			{ 'fname': 'Dor',
+				'lname': 'Rubin',
+				'email': 'd@r.com',
+				'numberPuppies': 1
+			},
+			{ 'fname': 'Dasha',
+				'lname': 'Murkovina',
+				'email': 'd@m.com',
+				'numberPuppies': 51
+			},
+			{ 'fname': 'Kelly',
+				'lname': 'Stevens',
+				'email': 'k@s.com',
+				'numberPuppies': 50
+			},
+			{ 'fname': 'Drew',
+				'lname': 'Garner',
+				'email': 'd@g.com',
+				'numberPuppies': 0
+			}
+		];
+
+		$scope.addUser = function(){
+			var defaultForm = {
+				'fname' : '',
+				'lname' : '',
+				'email' : '',
+				'numberPuppies' : ''
+			};
+			$scope.users.push({
+				'fname': $scope.user.fname,
+				'lname': $scope.user.lname,
+				'email': $scope.user.email,
+				'numberPuppies': $scope.user.numberPuppies
+			});
+			$scope.usersForm.$setPristine();
+			$scope.user = angular.copy(defaultForm);
+		};
+	}]);
